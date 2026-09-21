7cd67129fd3bfcc6b8c66b3bd4163bd3291b11e0
Ing. José Manuel Velazco Ochoa <minipeck01@hotmail.com>
2026-09-21 14:32:16 -0600

    feat: agregar estadisticas anonimas por pais

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@
 import { $, RM, initTheme, initBoot, initReveals, initScrollProgress, initNav,
          initModal, initAccordionResize, initProteccion } from "./core.js";
 import { initI18n, t } from "./i18n.js?v=geo-20260921p";
-import { initMapa } from "./mapa.js?v=geo-20260921p";
+import { initMapa } from "./mapa.js?v=geo-20260921q";
 import { initTrayectoria } from "./trayectoria.js?v=geo-20260921m";
 import { initCerts } from "./certificados.js?v=geo-20260921m";
 import { initVerificacion } from "./verificacion.js?v=geo-20260921m";
+import { initVisitas } from "./visitas.js?v=stats-20260921a";
 
 if ("scrollRestoration" in history) history.scrollRestoration = "manual";
 if (location.hash) history.replaceState(null, "", location.pathname + location.search);
@@ -87,3 +88,5 @@ initTrayectoria();
 initCerts();
 initVerificacion();
 initContacto();
+initVisitas();
+
